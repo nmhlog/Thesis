fbfd8c1260bdff34025b8b6eb922668295c62c1f
nmhlog <naufal.muhammad01@ui.ac.id>
2022-09-30 11:30:40 +0700

    edit train_backbone and config

--- train_backbone_dist.sh
+++ train_backbone_dist.sh
@@ -3,3 +3,6 @@
 bash dist_train.sh configs\training_different_feature\unet.yaml 4
 bash dist_train.sh configs\training_different_feature\unet_ASPP.yaml 4
 bash dist_train.sh configs\training_different_feature\unet_ASPP_DICELOSS.yaml 4
+bash dist_train.sh configs\training_different_feature\unet_coordinated.yaml 4
+bash dist_train.sh configs\training_different_feature\unet_ASPP_coordinated.yaml 4
+bash dist_train.sh configs\training_different_feature\unet_ASPP_DICELOSS_coordinated.yaml 4
