2156c27d9bed0dcb1590c08c4312cf34895eaa50
nmhlog <naufal.muhammad01@ui.ac.id>
2022-09-30 16:21:39 +0700

    update softgroup

--- train_backbone_dist.sh
+++ train_backbone_dist.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-bash dist_train.sh configs/training_different_feature/unet.yaml 4
-bash dist_train.sh configs/training_different_feature/unet_ASPP.yaml 4
-bash dist_train.sh configs/training_different_feature/unet_ASPP_DICELOSS.yaml 4
-bash dist_train.sh configs/training_different_feature/unet_coordinated.yaml 4
-bash dist_train.sh configs/training_different_feature/unet_ASPP_coordinated.yaml 4
-bash dist_train.sh configs/training_different_feature/unet_ASPP_DICELOSS_coordinated.yaml 4
+bash dist_train.sh configs/training_all/50X50_50/softgroup_stpls3d_unet_aspp_diceloss.yaml 4
+bash dist_train.sh configs/training_all/50X50_50/softgroup_stpls3d_unet_aspp.yaml 4
+bash dist_train.sh configs/training_all/50X50_50/softgroup_stpls3d_unet_aspp_diceloss.yaml 4
---
+++ train_dist.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bash dist_train.sh configs/training_different_feature/unet.yaml 4
+bash dist_train.sh configs/training_different_feature/unet_ASPP.yaml 4
+bash dist_train.sh configs/training_different_feature/unet_ASPP_DICELOSS.yaml 4
+bash dist_train.sh configs/training_different_feature/unet_coordinated.yaml 4
+bash dist_train.sh configs/training_different_feature/unet_ASPP_coordinated.yaml 4
+bash dist_train.sh configs/training_different_feature/unet_ASPP_DICELOSS_coordinated.yaml 4
