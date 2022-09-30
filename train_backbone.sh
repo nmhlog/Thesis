bac8e9c18c9ade6772966f560f5056e5ec9fd140
nmhlog <naufal.muhammad01@ui.ac.id>
2022-09-30 18:44:17 +0700

    update config

--- train_backbone.sh
+++ train_backbone.sh
@@ -4,7 +4,4 @@ CUDA_VISIBLE_DEVICES=5 python train.py configs/training_different_feature/unet.y
 CUDA_VISIBLE_DEVICES=5 python train.py configs/training_different_feature/unet_ASPP.yaml
 CUDA_VISIBLE_DEVICES=5 python train.py configs/training_different_feature/unet_ASPP_DICELOSS.yaml
 
-CUDA_VISIBLE_DEVICES=5 python train.py configs/training_different_feature/unet_coordinated.yaml
-CUDA_VISIBLE_DEVICES=5 python train.py configs/training_different_feature/unet_ASPP_coordinated.yaml
-CUDA_VISIBLE_DEVICES=5 python train.py configs/training_different_feature/unet_ASPP_DICELOSS_coordinated.yaml
 
