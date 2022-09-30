6e178d2d688458d36ae391f53e23b24a5384c701
nmhlog <naufal.muhammad01@ui.ac.id>
2022-09-30 11:27:07 +0700

    dist train

---
+++ train_backbone dist.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bash dist_train.sh configs\training_different_feature\unet.yaml 4
+bash dist_train.sh configs\training_different_feature\unet_ASPP.yaml 4
+bash dist_train.sh configs\training_different_feature\unet_ASPP_DICELOSS.yaml 4
