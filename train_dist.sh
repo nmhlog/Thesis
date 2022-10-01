5ce90ac26a71c82bf31e470d98e496e7dfb06aed
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-01 12:51:22 +0700

    update train_dist.sh

--- train_dist.sh
+++ train_dist.sh
@@ -3,11 +3,3 @@
 bash dist_train.sh configs/training_different_feature/unet_coordinated.yaml 4
 bash dist_train.sh configs/training_different_feature/unet_ASPP_coordinated.yaml 4
 bash dist_train.sh configs/training_different_feature/unet_ASPP_DICELOSS_coordinated.yaml 4
-
-bash dist_train_HAIS.sh configs/training_all/50X50_50/HAIS_stpls3d_unet.yaml 4
-bash dist_train_HAIS.sh configs/training_all/50X50_50/HAIS_stpls3d_unet_aspp.yaml 4
-bash dist_train_HAIS.sh configs/training_all/50X50_50/HAIS_stpls3d_unet_aspp_diceloss.yaml 4
-
-bash dist_train.sh configs/training_all/50X50_50/softgroup_stpls3d_unet.yaml 4
-bash dist_train.sh configs/training_all/50X50_50/softgroup_stpls3d_unet_aspp.yaml 4
-bash dist_train.sh configs/training_all/50X50_50/softgroup_stpls3d_unet_aspp_diceloss.yaml 4
