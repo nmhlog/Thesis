78d868a929f195cfe603034b586b820baf3c8afa
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-01 15:11:56 +0700

    training all setup update

--- train_backbone_dist.sh
+++ train_backbone_dist.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-bash dist_train.sh configs/training_different_feature/unet_only_coordinated.yaml 4
-bash dist_train.sh configs/training_different_feature/unet_ASPP_only_coordinated.yaml 4
-bash dist_train.sh configs/training_different_feature/unet_ASPP_DICELOSS_only_coordinated.yaml 4
+bash dist_train.sh configs/training_all/50X50_50/HAIS_stpls3d_unet.yaml 4
+bash dist_train.sh configs/training_all/50X50_50/HAIS_stpls3d_unet_aspp.yaml 4
+bash dist_train.sh configs/training_all/50X50_50/HAIS_stpls3d_unet_aspp_diceloss.yaml 4
+
+bash dist_train_HAIS.sh configs/training_all/50X50_50/softgroup_stpls3d_unet.yaml 4
+bash dist_train_HAIS.sh configs/training_all/50X50_50/softgroup_stpls3d_unet_aspp.yaml 4
+bash dist_train_HAIS.sh configs/training_all/50X50_50/softgroup_stpls3d_unet_aspp_diceloss.yaml 4
