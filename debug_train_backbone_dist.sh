66c82163ff11cd4243afe4ba2b21d9f92f5764de
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-02 16:26:55 +0700

    rerun

--- debug_train_backbone_dist.sh
+++ debug_train_backbone_dist.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cp buff_agg/5050hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
-cp buff_agg/5050hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
-bash rebuild_hais.sh > log_hais_installation.log
+
 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_50/semantic_unet_50x50_ASPP.yaml 4
 
