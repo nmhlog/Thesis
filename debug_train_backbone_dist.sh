c6399e0f998f0335ae1253d92a76de3792816ca4
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-02 16:56:43 +0700

    debug train backbone

--- debug_train_backbone_dist.sh
+++ debug_train_backbone_dist.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_50/semantic_unet_50x50_ASPP.yaml 4
+bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_50/semantic_unet_50x50.yaml 4
 
-bash dist_train-only_semantic.shconfigs/training_only_semantic_segmentation/50x50_50/semantic_unet_50x50.yaml 4
+bash dist_train.sh configs/training_backbone/50x50_50/unet.yaml 4
+bash dist_train.sh configs/training_backbone/50x50_50/unet_ASPP.yaml 4
+
+bash dist_train_HAIS.sh configs/training_all/50x50_50/HAIS_stpls3d_unet.yaml 4
+bash dist_train_HAIS.sh configs/training_all/50x50_50/HAIS_stpls3d_unet_aspp.yaml 4
