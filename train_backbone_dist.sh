fe1277fa23b298118946fe7358f502c7d5404992
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-02 10:22:09 +0700

    for 50x25

--- train_backbone_dist.sh
+++ train_backbone_dist.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_50/semantic_unet_50x50.yaml 4
-bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_50/semantic_unet_50x50_ASPP.yaml 4
-bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_50/semantic_unet_50x50_ASPP_DICELOSS.yaml 4
+bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25.yaml 4
+bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25_ASPP.yaml 4
 
-bash dist_train_HAIS.sh configs/training_all/50X50_50/HAIS_stpls3d_unet.yaml 4
-bash dist_train_HAIS.sh configs/training_all/50X50_50/HAIS_stpls3d_unet_aspp.yaml 4
-bash dist_train_HAIS.sh configs/training_all/50X50_50/HAIS_stpls3d_unet_aspp_diceloss.yaml 4
+bash dist_train.sh configs/training_backbone/50x50_25/unet_50x25.yaml 4
+bash dist_train.sh configs/training_backbone/50x50_25/unet_50x25_ASPP.yaml 4
 
-bash dist_train.sh configs/training_all/50X50_50/softgroup_stpls3d_unet.yaml 4
-bash dist_train.sh configs/training_all/50X50_50/softgroup_stpls3d_unet_aspp.yaml 4
-bash dist_train.sh configs/training_all/50X50_50/softgroup_stpls3d_unet_aspp_diceloss.yaml 4
+bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_50X25.yaml 4
+bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_aspp_50X25.yaml 4
+
+
+
+
 
 
