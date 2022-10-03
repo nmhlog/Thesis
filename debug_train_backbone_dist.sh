bafeb9c8f499041a325a98aab6a21536cf394085
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-03 10:27:49 +0700

    training 50x25

--- debug_train_backbone_dist.sh
+++ debug_train_backbone_dist.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+bash dist_train-only_semantic.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_50X25.yaml
+bash dist_train-only_semantic.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_aspp_50X25.yaml
 
-bash dist_train_HAIS.sh configs/training_all/50x50_50/HAIS_stpls3d_unet.yaml 4
-bash dist_train_HAIS.sh configs/training_all/50x50_50/HAIS_stpls3d_unet_aspp.yaml 4
+bash dist_train_HAIS.sh configs/training_backbone/50x50_25/unet_50x25.yaml 4
+bash dist_train_HAIS.sh configs/training_backbone/50x50_25/unet_50x25.yaml 4
+
+bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_50X25.yaml 4
+bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_aspp_50X25.yaml 4
