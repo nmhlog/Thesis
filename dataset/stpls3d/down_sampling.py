31ee4d51f9ca4017681e46ebc5b6af84c8977720
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-04 19:42:37 +0700

    update down sampling and prepare_data

--- dataset/stpls3d/down_sampling.py
+++ dataset/stpls3d/down_sampling.py
@@ -1,13 +1,8 @@
-# Copyright (c) Gorilla-Lab. All rights reserved.
-import argparse
-import glob
-import os
-import os.path as osp
-from random import sample
-
-import numpy as np
 import torch
-
+import tqdm
+from random import sample
+import numpy as np
+import glob
 
 def random_sample(coords: np.ndarray, colors: np.ndarray, semantic_labels: np.ndarray,
                   instance_labels: np.ndarray, ratio: float):
@@ -23,39 +18,31 @@ def random_sample(coords: np.ndarray, colors: np.ndarray, semantic_labels: np.nd
 
     return coords, colors, semantic_labels, instance_labels
 
-
-
-if __name__ == '__main__':
-    args = get_parser()
-
-    data_dir = args.data_dir
-    # voxelize or not
-    voxelize_flag = args.voxel_size is not None
-    if voxelize_flag:
-        print('processing: voxelize')
-        save_dir = f'{data_dir}_voxelize'
-    else:
-        print('processing: random sample')
-        save_dir = f'{data_dir}_sample'
-    os.makedirs(save_dir, exist_ok=True)
-
-    # for data_file in [osp.join(data_dir, "Area_6_office_17.pth")]:
-    for data_file in glob.glob(osp.join(data_dir, '*.pth')):
-        # for data_file in glob.glob(osp.join(data_dir, "*.pth")):
-        (coords, colors, semantic_labels, instance_labels, room_label,
-         scene) = torch.load(data_file)
-
-        if args.verbose:
-            print(f'processing: {scene}')
-
-        save_path = osp.join(save_dir, f'{scene}_inst_nostuff.pth')
-        if os.path.exists(save_path):
-            continue
-
-        if voxelize_flag:
-            raise NotImplementedError
-        else:
-            coords, colors, semantic_labels, instance_labels = \
-                random_sample(coords, colors, semantic_labels, instance_labels, args.ratio)
-
-        torch.save((coords, colors, semantic_labels, instance_labels, room_label, scene), save_path)
+if __name__ == "__main__":
+    data_folder = "val_100_100"
+    files = sorted(glob.glob(data_folder + '/*.pth'))
+    print('processing: {data_folder}')
+    for data_file in tqdm.tqdm(files):
+    # for data_file in glob.glob(osp.join(data_dir, "*.pth")):
+        (coords, colors, semantic_labels, instance_labels) = torch.load(data_file)
+        coords, colors, semantic_labels, instance_labels = random_sample(coords, colors, semantic_labels, instance_labels, 0.5)
+        torch.save((coords, colors, semantic_labels, instance_labels), data_file)
+    
+    
+    data_folder = "train_100_100"
+    files = sorted(glob.glob(data_folder + '/*.pth'))
+    print('processing: {data_folder}')
+    for data_file in tqdm.tqdm(files):
+    # for data_file in glob.glob(osp.join(data_dir, "*.pth")):
+        (coords, colors, semantic_labels, instance_labels) = torch.load(data_file)
+        coords, colors, semantic_labels, instance_labels = random_sample(coords, colors, semantic_labels, instance_labels, 0.5)
+        torch.save((coords, colors, semantic_labels, instance_labels), data_file)
+    
+    data_folder = "train_100_50"
+    files = sorted(glob.glob(data_folder + '/*.pth'))
+    print('processing: {data_folder}')
+    for data_file in tqdm.tqdm(files):
+    # for data_file in glob.glob(osp.join(data_dir, "*.pth")):
+        (coords, colors, semantic_labels, instance_labels) = torch.load(data_file)
+        coords, colors, semantic_labels, instance_labels = random_sample(coords, colors, semantic_labels, instance_labels, 0.5)
+        torch.save((coords, colors, semantic_labels, instance_labels), data_file)
