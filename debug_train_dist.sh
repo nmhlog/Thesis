f570204f28f4e6ab05b3b1e5a9c4f1542e3c35e4
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-04 18:24:15 +0700

    cuda visibel

--- debug_train_dist.sh
+++ debug_train_dist.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-bash dist_train-only_semantic.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_50X25.yaml 4
-bash dist_train-only_semantic.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_aspp_50X25.yaml 4
+CUDA_VISIBLE_DEVICES=2,3,4,5 bash dist_train-only_semantic.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_50X25.yaml 4
+CUDA_VISIBLE_DEVICES=2,3,4,5 bash dist_train-only_semantic.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_aspp_50X25.yaml 4
 
-bash dist_train_HAIS.sh configs/training_backbone/50x50_25/unet_50x25.yaml 4
-bash dist_train_HAIS.sh configs/training_backbone/50x50_25/unet_50x25.yaml 4
+CUDA_VISIBLE_DEVICES=2,3,4,5 bash dist_train_HAIS.sh configs/training_backbone/50x50_25/unet_50x25.yaml 4
+CUDA_VISIBLE_DEVICES=2,3,4,5 bash dist_train_HAIS.sh configs/training_backbone/50x50_25/unet_50x25.yaml 4
 
-bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_50X25.yaml 4
-bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_aspp_50X25.yaml 4
+CUDA_VISIBLE_DEVICES=2,3,4,5 bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_50X25.yaml 4
+CUDA_VISIBLE_DEVICES=2,3,4,5 bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_aspp_50X25.yaml 4
