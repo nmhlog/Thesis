d26dfb4a49c709d609e28e4399b68d692d4f9505
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-04 09:17:20 +0700

    reset setting

--- train_backbone_dist.sh
+++ train_backbone_dist.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25.yaml 4
-bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25_ASPP.yaml 4
-
-bash dist_train.sh configs/training_backbone/50x50_25/unet_50x25.yaml 4
-bash dist_train.sh configs/training_backbone/50x50_25/unet_50x25_ASPP.yaml 4
 
 bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_50X25.yaml 4
 bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_aspp_50X25.yaml 4
@@ -14,3 +9,4 @@ bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_aspp_50X
 
 
 
+s
