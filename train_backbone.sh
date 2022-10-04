f06de64ddab76e8878960aeb2ab5f2080c67ac45
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-04 11:06:26 +0700

    backbonetrain_.sh

--- train_backbone.sh
+++ train_backbone.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=5 python train.py configs/training_different_feature/unet.yaml
-CUDA_VISIBLE_DEVICES=5 python train.py configs/training_different_feature/unet_ASPP.yaml
-CUDA_VISIBLE_DEVICES=5 python train.py configs/training_different_feature/unet_ASPP_DICELOSS.yaml
+python train-HAIS.py configs/training_all/50x50_25/HAIS_stpls3d_unet_50X25.yaml 
+python train-HAIS.py configs/training_all/50x50_25/HAIS_stpls3d_unet_aspp_50X25.yaml 
 
 
