d7079acb70bc6a5508e6319052a816c556d5a286
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-04 19:53:38 +0700

    dataset\stpls3d\down_sampling.py

--- dataset/stpls3d/prepare_data.sh
+++ dataset/stpls3d/prepare_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo Preprocess data
-python dataset/stpls3d/down_sampling.py
+echo "down_sampling"
+python down_sampling.py
 python prepare_data_statistic_stpls3d.py --data_folder train_100_100 > note_weight100100.txt
 python prepare_data_statistic_stpls3d.py --data_folder train_100_50 > note_weight10050.txt
 
