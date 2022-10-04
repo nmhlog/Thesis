88171ed146681b8a036fcf2c233d7359726af918
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-04 17:45:15 +0700

    update .sh prepare data

--- dataset/stpls3d/prepare_data.sh
+++ dataset/stpls3d/prepare_data.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo Preprocess data
 python prepare_data_inst_instance_stpls3d.py
+python prepare_data_statistic_stpls3d.py --data_folder train_100_100 > note_weight100100.txt
+python prepare_data_statistic_stpls3d.py --data_folder train_100_50 > note_weight10050.txt
+
+
