45f9b1be44354cab51dc11f4b20c142d4113c504
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-05 15:07:43 +0700

    update resume

--- debug_train_dist.sh
+++ debug_train_dist.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25.yaml 4
-CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25_ASPP.yaml 4
-
-CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train_HAIS.sh configs/training_backbone/50x50_25/unet_50x25.yaml 4
-CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train_HAIS.sh configs/training_backbone/50x50_25/unet_50x25_ASPP.yaml 4
-
-CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_50X25.yaml 4
+CUDA_VISIBLE_DEVICES=4,5,6,7 bash resume_train-HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_50X25.yaml 4
 CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_aspp_50X25.yaml 4
