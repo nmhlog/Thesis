81e4a56943a1f95bb2c00b1150537a420ddd96be
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-06 05:17:29 +0700

    update resume

--- resume_train-HAIS.sh
+++ resume_train-HAIS.sh
@@ -4,4 +4,4 @@ GPUS=$2
 PORT=${PORT:-29500}
 RESUME =$3
 
-OMP_NUM_THREADS=1 torchrun --nproc_per_node=$GPUS --master_port=$PORT train-HAIS.py --dist $CONFIG ${@:3} --resume $RESUME
+OMP_NUM_THREADS=1 torchrun --nproc_per_node=$GPUS --master_port=$PORT train-HAIS.py --dist $CONFIG ${@:3} 
