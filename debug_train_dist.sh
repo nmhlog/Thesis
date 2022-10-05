47a462ea8f893548a11f3bb3ff4b17f0fd4e1681
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-06 05:13:59 +0700

    update resume bash

--- debug_train_dist.sh
+++ debug_train_dist.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 bash resume_train-HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_50X25.yaml 4
-CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_aspp_50X25.yaml 4
+CUDA_VISIBLE_DEVICES=4,5,6,7 bash resume_train-HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_50X25.yaml 4 work_dirs/HAIS_stpls3d_unet_50X25/epoch_104.pth
+CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train_HAIS.sh configs/training_all/50x50_25/HAIS_stpls3d_unet_aspp_50X25.yaml 4 work_dirs/HAIS_stpls3d_unet_aspp_50X25/epoch_10.pth
