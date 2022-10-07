345d7134e878e209e836fe401533930c52bdde36
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-08 05:16:32 +0700

    train 10050

--- debug_train_dist.sh
+++ debug_train_dist.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train_HAIS.sh configs/training_backbone/100x100_50/unet_100x50.yaml 4 
 CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_100x50.yaml 4 
