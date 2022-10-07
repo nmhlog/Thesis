46411a538463183e946cd0109ba75ebf7ae21082
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-07 20:32:33 +0700

    config and debug_train_dist

--- debug_train_dist.sh
+++ debug_train_dist.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-cp buff_agg/10050hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
-cp buff_agg/100100hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
-
-bash rebuild_hais.sh > log_hais_rebuild.log
-
-CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/100x100_50/semantic_unet_100x50.yaml 4 
-CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/100x100_50/semantic__unet_100x50_ASPP.yaml 4 
-
 CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train_HAIS.sh configs/training_backbone/100x100_50/unet_100x50.yaml 4 
 CUDA_VISIBLE_DEVICES=4,5,6,7 bash dist_train_HAIS.sh configs/training_backbone/100x100_50/unet_100x50_ASPP.yaml 4
 
