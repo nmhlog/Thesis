42ee4a6ed0ea0a908bf911a990c25853b9a8e876
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-14 11:14:33 +0700

    update training aspp

--- train_dist5050.sh
+++ train_dist5050.sh
@@ -22,9 +22,9 @@ CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_backbone/5
 
 echo "Training Instance"
 echo "Training HAIS_stpls3d_unet_aspp" 
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/50X50_50/HAIS_stpls3d_unet_aspp.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_aspp/epoch_10.pth
+CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/50X50_50/HAIS_stpls3d_unet_aspp.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_aspp/epoch_80.pth
  
 echo "Training HAIS_stpls3d_unet_ASPPv2" 
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/50X50_50/HAIS_stpls3d_unet_ASPPv2.yaml 4 
+CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/50X50_50/HAIS_stpls3d_unet_ASPPv2.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_ASPPv2_50x25/epoch_48.pth
 echo "Training HAIS_stpls3d_unet_ATTN_ASPP" 
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/50X50_50/HAIS_stpls3d_unet_ATTN_ASPP.yaml 4 
+CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/50X50_50/HAIS_stpls3d_unet_ATTN_ASPP.yaml 4 epoch_17.pth
