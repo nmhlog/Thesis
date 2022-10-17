2e1f05406e5ffd1f53a1b26fed4ee7a69730ef19
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-17 14:07:22 +0700

    reconfigure

--- TRAINING_ASPPV1_tapimalesganti.sh
+++ TRAINING_ASPPV1_tapimalesganti.sh
@@ -18,8 +18,8 @@ cp buff_agg/5025hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggreg
 cp buff_agg/5025hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
 echo "Rebuild HAIS"
 bash rebuild_hais.sh>log_hais_rebuild.log
-echo "Training semantic_unet ATTN_ASPP"
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25_ATTN_ASPP.yaml 4 
+# echo "Training semantic_unet ATTN_ASPP"
+# CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25_ATTN_ASPP.yaml 4 
 echo "Training Backbone"
 echo "Training unet_ATTN_ASPP"
 CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_backbone/50x50_25/unet_50x25_ATTN_ASPP.yaml 4 
