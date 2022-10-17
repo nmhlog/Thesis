8a210066e6300cd3599b30dde1e8a67d780933fe
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-17 14:07:36 +0700

    re

--- TRAINING_ASPPV1_tapimalesganti.sh
+++ TRAINING_ASPPV1_tapimalesganti.sh
@@ -3,7 +3,7 @@
 cp buff_agg/5050hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
 cp buff_agg/5050hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
 echo "Rebuild HAIS"
-# bash rebuild_hais.sh>log_hais_rebuild.log
+bash rebuild_hais.sh>log_hais_rebuild.log
 # echo "Training semantic_unet"
 # echo "Training semantic_unet ATTN_ASPP"
 # CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_50/semantic_unet_50x50_ATTN_ASPP.yaml 4 
