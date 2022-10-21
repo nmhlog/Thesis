8220046ba9d9a4ec0e0e1a43cfdbb81b1d464c92
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-21 10:47:07 +0700

    resume train_dist100

--- train_dist100.sh
+++ train_dist100.sh
@@ -32,40 +32,40 @@
 # CUDA_VISIBLE_DEVICES=1,2,3,4 bash dist_train_HAIS.sh configs/training_backbone/100x100_50/unet_100x50.yaml 4 
 # echo "Training unet_100x50"
 # CUDA_VISIBLE_DEVICES=1,2,3,4 bash dist_train_HAIS.sh configs/training_backbone/100x100_50/unet_100x50_ASPP.yaml 4 
-echo "Training backbone 100x50"
-echo "Training unet_100x50_ASPPv2"
-CUDA_VISIBLE_DEVICES=1,2,3,4 bash dist_train_HAIS.sh configs/training_backbone/100x100_50/unet_100x50_ASPPv2.yaml 4 --resume work_dirs/unet_100x50_ASPPv2/epoch_17.pth
-echo "Training unet_100x50_ATTN_ASPP"
-CUDA_VISIBLE_DEVICES=1,2,3,4 bash dist_train_HAIS.sh configs/training_backbone/100x100_50/unet_100x50_ATTN_ASPP.yaml 4 
-echo "Training Semantic 100x50"
-echo "Training semantic_unet 100x50"
-CUDA_VISIBLE_DEVICES=1,2,3,4 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/100x100_50/semantic_unet_100x50.yaml 4 
+# echo "Training backbone 100x50"
+# echo "Training unet_100x50_ASPPv2"
+# CUDA_VISIBLE_DEVICES=1,2,3,4 bash dist_train_HAIS.sh configs/training_backbone/100x100_50/unet_100x50_ASPPv2.yaml 4 --resume work_dirs/unet_100x50_ASPPv2/epoch_17.pth
+# echo "Training unet_100x50_ATTN_ASPP"
+# CUDA_VISIBLE_DEVICES=1,2,3,4 bash dist_train_HAIS.sh configs/training_backbone/100x100_50/unet_100x50_ATTN_ASPP.yaml 4 
+# echo "Training Semantic 100x50"
+# echo "Training semantic_unet 100x50"
+# CUDA_VISIBLE_DEVICES=1,2,3,4 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/100x100_50/semantic_unet_100x50.yaml 4 
 
-echo "Rebuild HAIS 100x100"
-cp buff_agg/100100hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
-cp buff_agg/100100hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
-bash rebuild_hais.sh>log_hais_rebuild.log
+# echo "Rebuild HAIS 100x100"
+# cp buff_agg/100100hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
+# cp buff_agg/100100hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
+# bash rebuild_hais.sh>log_hais_rebuild.log
 echo "Training Instance"
 echo "Training HAIS_stpls3d_unet_100x100"
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_100x100.yaml 4 
+CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_100x100.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_100x100/epoch_47.pth
 echo "Training HAIS_stpls3d_unet_aspp_100x100"
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_aspp_100x100.yaml 4 
+CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_aspp_100x100.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_aspp_100x100/epoch_28.pth
 echo "Training HAIS_stpls3d_unet_ASPPv2_100x100"
 CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_ASPPv2_100x100.yaml 4 
 echo "Training HAIS_stpls3d_unet_ATTN_ASPP_100x100"
 CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_ATTN_ASPP_100x100.yaml 4 
 
-echo "Rebuild HAIS 100x50"
-cp buff_agg/10050hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
-cp buff_agg/10050hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
-bash rebuild_hais.sh>log_hais_rebuild.log
+# echo "Rebuild HAIS 100x50"
+# cp buff_agg/10050hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
+# cp buff_agg/10050hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
+# bash rebuild_hais.sh>log_hais_rebuild.log
 
-echo "Training Instance 100x50"
-echo "Training HAIS_stpls3d_unet_100x50"
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_100x50.yaml 4 
-echo "Training HAIS_stpls3d_unet_aspp_100x50"
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_aspp_100x50.yaml 4 
-echo "Training HAIS_stpls3d_unet_ASPPv2_100x50"
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_ASPPv2_100x50.yaml 4 
-echo "Training HAIS_stpls3d_unet_ATTN_ASPP_100x50"
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_ATTN_ASPP_100x50.yaml 4 
+# echo "Training Instance 100x50"
+# echo "Training HAIS_stpls3d_unet_100x50"
+# CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_100x50.yaml 4 
+# echo "Training HAIS_stpls3d_unet_aspp_100x50"
+# CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_aspp_100x50.yaml 4 
+# echo "Training HAIS_stpls3d_unet_ASPPv2_100x50"
+# CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_ASPPv2_100x50.yaml 4 
+# echo "Training HAIS_stpls3d_unet_ATTN_ASPP_100x50"
+# CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_ATTN_ASPP_100x50.yaml 4 
