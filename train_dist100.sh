8edb37ca26a75805bf915422ab52f2678b2bb08f
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-22 22:07:39 +0700

    resume train dist 100

--- train_dist100.sh
+++ train_dist100.sh
@@ -46,12 +46,12 @@
 # cp buff_agg/100100hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
 # bash rebuild_hais.sh>log_hais_rebuild.log
 echo "Training Instance"
-echo "Training HAIS_stpls3d_unet_100x100"
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_100x100.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_100x100/epoch_80.pth
+# echo "Training HAIS_stpls3d_unet_100x100"
+# CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_100x100.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_100x100/epoch_80.pth
 echo "Training HAIS_stpls3d_unet_aspp_100x100"
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_aspp_100x100.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_aspp_100x100/epoch_28.pth
+CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_aspp_100x100.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_aspp_100x100/epoch_71.pth
 echo "Training HAIS_stpls3d_unet_ASPPv2_100x100"
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_ASPPv2_100x100.yaml 4 
+CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_ASPPv2_100x100.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_ASPPv2_100x100/epoch_11.pth
 echo "Training HAIS_stpls3d_unet_ATTN_ASPP_100x100"
 CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_ATTN_ASPP_100x100.yaml 4 
 
