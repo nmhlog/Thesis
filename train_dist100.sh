f33d9b78920b975a1d726d93a554770d9a0e25f0
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-25 22:24:49 +0700

    train100

--- train_dist100.sh
+++ train_dist100.sh
@@ -53,16 +53,16 @@ echo "Training Instance"
 # echo "Training HAIS_stpls3d_unet_ASPPv2_100x100"
 # CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_ASPPv2_100x100.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_ASPPv2_100x100/epoch_103.pth
 echo "Training HAIS_stpls3d_unet_ATTN_ASPP_100x100"
-CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_ATTN_ASPP_100x100.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_ATTN_ASPP_100x100/epoch_47.pth
+CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_ATTN_ASPP_100x100.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_ATTN_ASPP_100x100/epoch_64.pth
 
-# echo "Rebuild HAIS 100x50"
-# cp buff_agg/10050hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
-# cp buff_agg/10050hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
-# bash rebuild_hais.sh>log_hais_rebuild.log
+echo "Rebuild HAIS 100x50"
+cp buff_agg/10050hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
+cp buff_agg/10050hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
+bash rebuild_hais.sh>log_hais_rebuild.log
 
-# echo "Training Instance 100x50"
-# echo "Training HAIS_stpls3d_unet_100x50"
-# CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_100x50.yaml 4 
+echo "Training Instance 100x50"
+echo "Training HAIS_stpls3d_unet_100x50"
+CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_100x50.yaml 4 
 # echo "Training HAIS_stpls3d_unet_aspp_100x50"
 # CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_aspp_100x50.yaml 4 
 # echo "Training HAIS_stpls3d_unet_ASPPv2_100x50"
