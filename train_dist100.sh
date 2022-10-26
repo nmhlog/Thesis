a5947144c49c2330a7bc6302e3b67188359fb98c
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-27 00:25:35 +0700

    update setting

--- train_dist100.sh
+++ train_dist100.sh
@@ -53,7 +53,7 @@ echo "Training Instance"
 # echo "Training HAIS_stpls3d_unet_ASPPv2_100x100"
 # CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_ASPPv2_100x100.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_ASPPv2_100x100/epoch_103.pth
 echo "Training HAIS_stpls3d_unet_ATTN_ASPP_100x100"
-CUDA_VISIBLE_DEVICES=0,2,3,4 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_ATTN_ASPP_100x100.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_ATTN_ASPP_100x100/epoch_105.pth
+CUDA_VISIBLE_DEVICES=0,1,2,3 bash dist_train_HAIS.sh configs/training_all/100x100_100/HAIS_stpls3d_unet_ATTN_ASPP_100x100.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_ATTN_ASPP_100x100/epoch_105.pth
 
 echo "Rebuild HAIS 100x50"
 cp buff_agg/10050hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
@@ -62,10 +62,10 @@ bash rebuild_hais.sh>log_hais_rebuild.log
 
 echo "Training Instance 100x50"
 echo "Training HAIS_stpls3d_unet_100x50"
-CUDA_VISIBLE_DEVICES=0,2,3,4 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_100x50.yaml 4 
+CUDA_VISIBLE_DEVICES=0,1,2,3 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_100x50.yaml 4 
 echo "Training HAIS_stpls3d_unet_aspp_100x50"
-CUDA_VISIBLE_DEVICES=0,2,3,4 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_aspp_100x50.yaml 4 
+CUDA_VISIBLE_DEVICES=0,1,2,3 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_aspp_100x50.yaml 4 
 echo "Training HAIS_stpls3d_unet_ASPPv2_100x50"
-CUDA_VISIBLE_DEVICES=0,2,3,4 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_ASPPv2_100x50.yaml 4 
+CUDA_VISIBLE_DEVICES=0,1,2,3 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_ASPPv2_100x50.yaml 4 
 echo "Training HAIS_stpls3d_unet_ATTN_ASPP_100x50"
-CUDA_VISIBLE_DEVICES=0,2,3,4 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_ATTN_ASPP_100x50.yaml 4 
+CUDA_VISIBLE_DEVICES=0,1,2,3 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_ATTN_ASPP_100x50.yaml 4 
