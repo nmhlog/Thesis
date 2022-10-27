8a174aafbf01160548c73f235ca4f9feed447ccf
nmhlog <naufal.muhammad01@ui.ac.id>
2022-10-27 15:03:05 +0700

    update train_dist

--- train_dist100.sh
+++ train_dist100.sh
@@ -62,7 +62,7 @@ echo "Training Instance"
 
 echo "Training Instance 100x50"
 echo "Training HAIS_stpls3d_unet_100x50"
-CUDA_VISIBLE_DEVICES=0,1,2,3 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_100x50.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_100x50/epoch_4.pth
+CUDA_VISIBLE_DEVICES=0,1,2,3 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_100x50.yaml 4 --resume work_dirs/HAIS_stpls3d_unet_100x50/epoch_15.pth
 echo "Training HAIS_stpls3d_unet_aspp_100x50"
 CUDA_VISIBLE_DEVICES=0,1,2,3 bash dist_train_HAIS.sh configs/training_all/100x100_50/HAIS_stpls3d_unet_aspp_100x50.yaml 4 
 echo "Training HAIS_stpls3d_unet_ASPPv2_100x50"
