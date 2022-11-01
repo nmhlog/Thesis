29b66d88e9d0758dbf0ccc79c88a8bd42a2a8026
nmhlog <naufal.muhammad01@ui.ac.id>
2022-11-01 17:52:29 +0700

    update config2

--- train_semantic_segmentation_model.py
+++ train_semantic_segmentation_model.py
@@ -29,7 +29,7 @@ def get_args():
     parser.add_argument('--dist', action='store_true', help='run with distributed parallel')
     parser.add_argument('--resume', type=str, help='path to resume from')
     parser.add_argument('--work_dir', type=str, help='working directory')
-    parser.add_argument('--skip_validate', action='store_true',default=True, help='skip validation')
+    parser.add_argument('--skip_validate', action='store_true',default=False, help='skip validation')
     args = parser.parse_args()
     return args
 
