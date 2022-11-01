859c3bcddaad8996d424c81f89e97af42587f20b
nmhlog <naufal.muhammad01@ui.ac.id>
2022-11-01 17:29:52 +0700

    update ATTN-asppv1

--- train_dist5025.sh
+++ train_dist5025.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# cp buff_agg/5025hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
-# cp buff_agg/5025hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
-# echo "Rebuild HAIS"
-# bash rebuild_hais.sh>log_hais_rebuild.log
+cp buff_agg/5025hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
+cp buff_agg/5025hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
+echo "Rebuild HAIS"
+bash rebuild_hais.sh>log_hais_rebuild.log
+echo "Training semantic_unet ATTN_ASPP"
+CUDA_VISIBLE_DEVICES=3,4,5,6 python train_semantic_segmentation_model.py configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25_ATTN_ASPP.yaml 4 
+
+
 # echo "Training Semantic"
 # echo "Training semantic_unet ASPP"
 # CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25_ASPP.yaml 4 
 # echo "Training semantic_unet ASPPv2"
 # CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25_ASPPv2.yaml 4 
-# echo "Training semantic_unet ATTN_ASPP"
-# CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25_ATTN_ASPP.yaml 4 
 # echo "Training Backbone"
 # echo "Training unet_ASPP"
 # CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train_HAIS.sh configs/training_backbone/50x50_25/unet_50x25_ASPP.yaml 4 
--- train_dist5050.sh
+++ train_dist5050.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# cp buff_agg/5050hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
-# cp buff_agg/5050hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
-# echo "Rebuild HAIS"
-# bash rebuild_hais.sh>log_hais_rebuild.log
+cp buff_agg/5050hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
+cp buff_agg/5050hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
+echo "Rebuild HAIS"
+bash rebuild_hais.sh>log_hais_rebuild.log
+train_semantic_segmentation_model.py
 # echo "Training Semantic"
 # echo "Training semantic_unet_50x50_ASPP"
 # CUDA_VISIBLE_DEVICES=3,4,5,6 bash dist_train-only_semantic.sh configs/training_only_semantic_segmentation/50x50_50/semantic_unet_50x50_ASPP.yaml 4 
