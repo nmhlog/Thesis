bbeb9307cb0bf411440cea059240a39154b3b7ba
nmhlog <naufal.muhammad01@ui.ac.id>
2022-11-03 21:15:35 +0700

    COMMET REBUILD_HAIS

--- train_5025.sh
+++ train_5025.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp buff_agg/5025hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
-cp buff_agg/5025hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
-echo "Rebuild HAIS"
-bash rebuild_hais.sh
+# cp buff_agg/5025hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
+# cp buff_agg/5025hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
+# echo "Rebuild HAIS"
+# bash rebuild_hais.sh
 CUDA_VISIBLE_DEVICES=2 python train_semantic_segmentation_model.py configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25.yaml 
 CUDA_VISIBLE_DEVICES=2 python train_semantic_segmentation_model.py configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25_ASPPv2.yaml 
 CUDA_VISIBLE_DEVICES=2 python train_semantic_segmentation_model.py configs/training_only_semantic_segmentation/50x50_25/semantic_unet_50x25_ATTN_ASPP.yaml 
