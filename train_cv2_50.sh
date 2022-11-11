c13ed99150105cb17d3d78caff01619fd056f660
nmhlog <naufal.muhammad01@ui.ac.id>
2022-11-11 15:29:59 +0700

    update traincv2_

--- train_cv2_50.sh
+++ train_cv2_50.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "Rebuild HAIS"
-cp buff_agg/CrossValidation/cv2hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
-cp buff_agg/CrossValidation/cv2hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
+# echo "Rebuild HAIS"
+# cp buff_agg/CrossValidation/cv2hierarchical_aggregation.cu lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cu
+# cp buff_agg/CrossValidation/cv2hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggregation/hierarchical_aggregation.cpp
 
-echo "Rebuild HAIS"
-bash rebuild_hais.sh > rebuild_hais_cv2_5050.log
+# echo "Rebuild HAIS"
+# bash rebuild_hais.sh > rebuild_hais_cv2_5050.log
 echo "Training semantic"
-CUDA_VISIBLE_DEVICES=2 python train_semantic_segmentation_model.py configs/Cross Validation/5050/CV2/cv2_5050_semantic_unet.yaml
-CUDA_VISIBLE_DEVICES=2 python train_semantic_segmentation_model.py configs/Cross Validation/5050/CV2/cv2_5050_semantic_unet_ASPPv2.yaml
-CUDA_VISIBLE_DEVICES=2 python train_semantic_segmentation_model.py configs/Cross Validation/5050/CV2/cv2_5050_semantic_unet_ATTN_ASPP.yaml
+CUDA_VISIBLE_DEVICES=7 python train_semantic_segmentation_model.py configs/Cross Validation/5050/CV2/cv2_5050_semantic_unet.yaml
+CUDA_VISIBLE_DEVICES=7 python train_semantic_segmentation_model.py configs/Cross Validation/5050/CV2/cv2_5050_semantic_unet_ASPPv2.yaml
+CUDA_VISIBLE_DEVICES=7 python train_semantic_segmentation_model.py configs/Cross Validation/5050/CV2/cv2_5050_semantic_unet_ATTN_ASPP.yaml
 
