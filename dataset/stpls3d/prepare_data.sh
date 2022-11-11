a9983c3abaa25eedf282f4a56d33850fae80f64a
nmhlog <naufal.muhammad01@ui.ac.id>
2022-11-11 14:53:24 +0700

    update bug cv2

--- dataset/stpls3d/prepare_data.sh
+++ dataset/stpls3d/prepare_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "down_sampling"
 python prepare_data_inst_instance_stpls3d.py
-python prepare_data_statistic_stpls3d.py --data_folder traincv2_50_50 > note_weight100100.txt
-python prepare_data_statistic_stpls3d.py --data_folder traincv2_50_25 > note_weight10050.txt
+python prepare_data_statistic_stpls3d.py --data_folder traincv2_50_50 > note_weighttraincv2_50_50.txt
+python prepare_data_statistic_stpls3d.py --data_folder traincv2_50_25 > note_weighttraincv2_50_25.txt
 
 
