c7b48f3887a53928530373172dcc8d9f3574ee50
nmhlog <naufal.muhammad01@ui.ac.id>
2022-11-11 14:04:03 +0700

    update prepare_data.sh

--- dataset/stpls3d/prepare_data.sh
+++ dataset/stpls3d/prepare_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "down_sampling"
-python down_sampling.py
+python prepare_data_inst_instance_stpls3d.py
 python prepare_data_statistic_stpls3d.py --data_folder traincv2_50_50 > note_weight100100.txt
 python prepare_data_statistic_stpls3d.py --data_folder traincv2_50_25 > note_weight10050.txt
 
