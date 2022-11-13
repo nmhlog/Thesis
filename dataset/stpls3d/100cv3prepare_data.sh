c4b27c03183a7197730f5508cb6e2f0e559e3546
nmhlog <naufal.muhammad01@ui.ac.id>
2022-11-13 16:45:33 +0700

    update 100cv3prepare_data.sh

--- dataset/stpls3d/100cv3prepare_data.sh
+++ dataset/stpls3d/100cv3prepare_data.sh
@@ -6,5 +6,5 @@ python 100cv3prepare_data_inst_instance_stpls3d.py
 python prepare_data_statistic_stpls3d.py --data_folder traincv3_100_100 > note_weighttraincv3_100_100.txt
 python prepare_data_statistic_stpls3d.py --data_folder traincv3_100_50 > note_weighttraincv3_100_50.txt
 rm 100cv3prepare_data_inst_instance_stpls3d.py
-
-zip data_cv3_100.zip val_cv3_100 traincv3_100_100/ traincv3_100_50/ note_weighttraincv3_100_50.txt note_weighttraincv3_100_100.txt
+python down_sampling.py
+zip -r data_cv3_100.zip val_cv3_100/ val_cv3_100_sampling/ traincv3_100_100/ traincv3_100_50/ note_weighttraincv3_100_50.txt note_weighttraincv3_100_100.txt
