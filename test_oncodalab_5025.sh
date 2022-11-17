a04632f9fbf16c2645680c5778c0aa42996cfca1
nmhlog <naufal.muhammad01@ui.ac.id>
2022-11-17 23:07:21 +0700

    update test_oncodalab

--- test_oncodalab_5025.sh
+++ test_oncodalab_5025.sh
@@ -3,5 +3,5 @@ cp buff_agg/5025hierarchical_aggregation.cpp lib/hais_ops/src/hierarchical_aggre
 echo "Rebuild HAIS 50-25"
 bash rebuild_hais.sh>log_hais_rebuild5025.log
 
-CUDA_VISIBLE_DEVICES=2 python test-HAIS-oncodalab-test.py configs/Test/50x50_25/HAIS_stpls3d_unet_50x25.yaml pretrain/5025/HAIS_stpls3d_unet_50X25/epoch_96.pth --out out/HAIS_stpls3d_unet_50X25/
-CUDA_VISIBLE_DEVICES=2 python test-HAIS-oncodalab-test.py configs/Test/50x50_25/HAIS_stpls3d_unet_ATTN_ASPP_50x25.yaml pretrain/5025/HAIS_stpls3d_unet_ATTN_ASPP_50x25/epoch_104.pth --out out/HAIS_stpls3d_unet_ATTN_ASPP_50x25/
+CUDA_VISIBLE_DEVICES=2 python test-HAIS-oncodalab.py configs/Test/50x50_25/HAIS_stpls3d_unet_50x25.yaml pretrain/5025/HAIS_stpls3d_unet_50X25/epoch_96.pth --out out/HAIS_stpls3d_unet_50X25/
+CUDA_VISIBLE_DEVICES=2 python test-HAIS-oncodalab.py configs/Test/50x50_25/HAIS_stpls3d_unet_ATTN_ASPP_50x25.yaml pretrain/5025/HAIS_stpls3d_unet_ATTN_ASPP_50x25/epoch_104.pth --out out/HAIS_stpls3d_unet_ATTN_ASPP_50x25/
