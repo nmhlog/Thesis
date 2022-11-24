1b0047ae245ab6eec31307fe11b3d6db5421a783
nmhlog <naufal.muhammad01@ui.ac.id>
2022-11-25 02:28:15 +0700

    update prepare

--- dataset/stpls3d/prepare_data.sh
+++ dataset/stpls3d/prepare_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Analyst Points And Size"
 
-python analyst_data_statistic_stpls3d.py --data_folder train_50_50 > statistic_analyst_50_50.txt
+python analyst_data_statistic_stpls3d.py --data_folder train > statistic_analyst_50_50.txt
 python analyst_data_statistic_stpls3d.py --data_folder train_50_25 > statistic_analyst_50_25.txt
 python analyst_data_statistic_stpls3d.py --data_folder val > statistic_analyst_val_50_50.txt
 
