9d5ac203afb40a7d5c3394b948c1ceea964aa319
nmhlog <naufal.muhammad01@ui.ac.id>
2022-11-25 02:19:22 +0700

    update anlayst dataset

---
+++ dataset/stpls3d/analyst_data_statistic_stpls3d.py
@@ -0,0 +1,81 @@
+import glob
+import math
+import os
+import argparse
+import numpy as np
+import torch
+
+# data_folder = "train"
+# files = sorted(glob.glob(data_folder + '/*.pth'))
+# numclass = 15
+
+def get_args():
+    parser = argparse.ArgumentParser('')
+    parser.add_argument('--data_folder', type=str, help='Folder files',default="train")
+    parser.add_argument('--numclass',type=int,  help='number of class',default=15)
+    
+    args = parser.parse_args()
+    return args
+
+def stat_cal(data_folder="train",numclass=15):
+    files=sorted(glob.glob(data_folder + '/*.pth'))
+    semanticIDs = []
+    for i in range(numclass):
+        semanticIDs.append(i)
+
+    class_numpoint_mean_dict = {}
+    class_radius_mean = {}
+    for semanticID in semanticIDs:
+        class_numpoint_mean_dict[semanticID] = []
+        class_radius_mean[semanticID] = []
+    num_points_semantic = np.array([0 for i in range(numclass)])
+
+    for file in files:
+        coords, colors, sem_labels, instance_labels = torch.load(file)
+        points = np.concatenate(
+            [coords, colors, sem_labels[:, None].astype(int), instance_labels[:, None].astype(int)],
+            axis=1)
+        for semanticID in semanticIDs:
+            singleSemantic = points[np.where(points[:, 6] == semanticID)]
+            uniqueInstances, counts = np.unique(singleSemantic[:, 7], return_counts=True)
+            for count in counts:
+                class_numpoint_mean_dict[semanticID].append(count)
+            allRadius = []
+            for uniqueInstance in uniqueInstances:
+                eachInstance = singleSemantic[np.where(singleSemantic[:, 7] == uniqueInstance)]
+                radius = (np.max(eachInstance, axis=0) - np.min(eachInstance, axis=0)) / 2
+                radius = math.sqrt(radius[0]**2 + radius[1]**2 + radius[2]**2)
+                class_radius_mean[semanticID].append(radius)
+
+        uniqueSemantic, semanticCount = np.unique(points[:, 6], return_counts=True)
+        uniqueSemanticCount = np.array([0 for i in range(numclass)])
+        uniqueSemantic = uniqueSemantic.astype(int)
+        indexOf100 = np.where(uniqueSemantic == -100)
+        semanticCount = np.delete(semanticCount, indexOf100)
+        uniqueSemantic = np.delete(uniqueSemantic, indexOf100)
+        uniqueSemanticCount[uniqueSemantic] = semanticCount
+        num_points_semantic += uniqueSemanticCount
+
+    class_numpoint_mean_list = []
+    class_radius_mean_list = []
+    for semanticID in semanticIDs:
+        class_numpoint_mean_list.append(
+            sum(class_numpoint_mean_dict[semanticID]) * 1.0 / len(class_numpoint_mean_dict[semanticID]))
+        class_radius_mean_list.append(
+            sum(class_radius_mean[semanticID]) / len(class_radius_mean[semanticID]))
+
+    print('Using the printed list in hierarchical_aggregation.cpp for class_numpoint_mean_dict: ')
+    print([float('{0:0.0f}'.format(i)) for i in class_numpoint_mean_list], sep=',')
+    print('Using the printed list in hierarchical_aggregation.cu for class_radius_mean: ')
+    print([float('{0:0.2f}'.format(i)) for i in class_radius_mean_list], sep='')
+
+    # make ground to 1 the make building to 1
+    maxSemantic = np.max(num_points_semantic)
+    num_points_semantic = maxSemantic / num_points_semantic
+    num_points_semantic = num_points_semantic / num_points_semantic[1]
+    print('Using the printed list in hais_run_stpls3d.yaml for class_weight')
+    print([float('{0:0.2f}'.format(i)) for i in num_points_semantic], sep='')
+
+if __name__=="__main__":
+    args = get_args()
+    stat_cal(args.data_folder,args.numclass)
